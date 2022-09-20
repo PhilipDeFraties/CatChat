2a41235bc53b4d5f84f20c8deebd244e66ddf12f
Philip DeFraties <65036872+PhilipDeFraties@users.noreply.github.com>
2022-09-19 22:40:09 -0600

    Adds logic to chat class to enable message creation functionality, adds logic to app.js to allow username update and stores / checks local storage

---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+// dom queries
+const chatList = document.querySelector('.chat-list')
+const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name')
+const updateMsg = document.querySelector('.update-mssg')
+
+// add a new chat
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(err => console.log(err))
+})
+
+// update username
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault();
+    // update name via chatroom class
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName)
+    // reset the form
+    newNameForm.reset();
+    // show then hide the update message
+    updateMsg.innerText = `Your name was updated to ${newName}`;
+    setTimeout(() => updateMsg.innerText = '', 3000)
+})
+
+// check local storage for a username
+const username = localStorage.username ? localStorage.username : 'anon'
+
+// class instances
+const chatUi = new ChatUi(chatList);
+const chatroom = new Chatroom('general', username);
+
+chatroom.getChats(data => chatUi.render(data))
--- scripts/chat.js
+++ scripts/chat.js
@@ -3,9 +3,9 @@ class Chatroom {
         this.room = room;
         this.username = username;
         this.chats = db.collection('chats');
+        this.unsub
     }
     async addChat(message){
-        // format a chat object
         const now = new Date();
         const chat = {
             message,
@@ -17,10 +17,27 @@ class Chatroom {
         const response = await this.chats.add(chat);
         return response;
     }
+    getChats(callback){
+        this.chats
+            .where('room', '==', this.room)
+            .orderBy('created_at')
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if (change.type === 'added') {
+                        callback(change.doc.data());
+                    }
+                })
+            })
+    }
+    updateName(username){
+        this.username = username;
+        localStorage.setItem('username', username)
+    }
+    updateRoom(room){
+        this.room = room;
+        console.log('room updated');
+        if (this.unsub){
+            this.unsub();
+        }
+    }
 }
-
-const chatroom = new Chatroom('gaming', 'shadow');
-
-chatroom.addChat('hello cats')
-    .then(() => console.log('chat added'))
-    .catch(err => console.log(err));
